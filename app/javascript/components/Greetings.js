2cd9cb3903b214715485d91a4007d017e9ddeab5
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-14 22:16:04 -0500

    fetch data from api using useState

--- app/javascript/components/Greetings.js
+++ app/javascript/components/Greetings.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Greetings = () => {
-  return (
-    <div>Greetings</div>
-  )
-}
+  const [greetingsData, setGreetingsData] = useState([]);
+  const url = "http://localhost:3000/api/v1/messages";
 
-export default Greetings
+  useEffect(() => {
+    axios.get(url)
+      .then((res) => {
+        setGreetingsData(res.data);
+      })
+      .catch((err) => {
+        console.error('Error fetching data:', err);
+      });
+  }, []);
+
+  console.log(greetingsData);
+
+  return (
+    <div>
+      <h2>Greetings</h2>
+      <p>{greetingsData.greeting}</p>
+    </div>
+  );
+};
+
+export default Greetings;
