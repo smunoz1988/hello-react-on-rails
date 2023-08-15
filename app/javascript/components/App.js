b171ee4746255de5d81f1111e39ce0b54f79b4f3
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-14 23:44:45 -0500

    set greeting route on App

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Greetings from './Greetings';
 
 function App() {
   return (
-    <>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/Greetings">Greetings</NavLink>
-            </li>
-          </ul>
-          </nav>
-      </div>
-
       <Routes>
-          <Route path="Greetings" element={<Greetings />} />
+          <Route path="/" element={<Greetings />} />
       </Routes>
-    </>
   );
 }
 
