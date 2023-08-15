e402579ef6f787a06b89aad9e48a7fe962e6874e
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-14 23:44:30 -0500

    add store to provider

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,12 +5,16 @@ import "./controllers"
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />,
-    </BrowserRouter>,
+    <Provider store={store}>  
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
